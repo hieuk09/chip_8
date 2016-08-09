d3355975043456cbc778e741599c0c465894e3b4
Hieu Nguyen <hieuk09@gmail.com>
2016-08-09 08:56:14 +0700

    [f] WIP - renderer

--- lib/chip_8/main.rb
+++ lib/chip_8/main.rb
@@ -1,14 +1,54 @@
+require 'matrix'
+
 module Chip8
   class Main
-    attr_reader :renderer, :memory
+    attr_reader :memory, :registers, :delay_timer, :stack,
+      :display, :width, :height
+    attr_accessor :draw_flag, :program_counter, :register_i, :running
 
-    def initialize(renderer: nil)
-      @renderer = renderer
+    def initialize
       @memory = Array.new(0x1000)
       @registers = Array.new(16)
       @stack = Array.new
       @program_counter = 0
-      @stack_pointer = nil
+      @draw_flag = false
+      @register_i = nil
+      @delay_timer = 0
+      @running = false
+      @width = 64
+      @height = 32
+      @display = Matrix.build(width, height) { 0 }
+
+      (0..0x1000 - 1).each do |i|
+        memory[i] = 0
+      end
+
+      hex_chars = [
+        0xF0, 0x90, 0x90, 0x90, 0xF0, # 0
+        0x20, 0x60, 0x20, 0x20, 0x70, # 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, # 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, # 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, # 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, # 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, # 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, # 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, # 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, # 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, # A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, # B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, # C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, # D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, # E
+        0xF0, 0x80, 0xF0, 0x80, 0x80 # F
+      ]
+
+      (0..hex_chars.length - 1).each do |i|
+        memory[i] = hex_chars[i]
+      end
+
+      (0..15).each do |i|
+        registers[i] = 0
+      end
     end
 
     def open(rom_path)
@@ -21,17 +61,14 @@ module Chip8
     end
 
     def start
-      if renderer
-        renderer.show
-        run
-      else
-        raise NoRendererError
-      end
+      self.running = true
     end
 
-    private
-
     def run
+      if program_counter >= 0x1000
+        self.running = false
+        return
+      end
       opcode = (memory[program_counter] << 8) | memory[program_counter + 1]
       self.program_counter += 2
 
@@ -44,18 +81,16 @@ module Chip8
       when 0x0000
         case opcode
         when 0x00E0
-          renderer.clear
+          # clear
         when 0x00EE
-          self.program_counter = stack[stack_pointer]
-          self.stack_pointer -= 1
+          self.program_counter = stack.pop
         end
 
       when 0x1000 # JMP
         self.program_counter = sub_routine_pointer
 
       when 0x2000 # CALL
-        stack[stack_pointer] = program_counter
-        self.stack_pointer += 1
+        stack.push(program_counter)
         self.program_counter = sub_routine_pointer
 
       when 0x3000 # SE - skip if equal
@@ -156,7 +191,25 @@ module Chip8
 
           (0..7).each do |j|
             if (data & 0x80) > 0
-              set_pixel(x + j, y + i)
+              # set pixel
+              a = x + j
+              b = y + i
+
+              if a > width
+                a -= width
+              elsif a < 0
+                a += width
+              end
+
+              if b > height
+                b -= height
+              elsif b < 0
+                b += height
+              end
+
+              value = display[a, b] ^ 1
+              display.send(:'[]=', a, b, display)
+
               registers[0xF] = 1
             end
             data <<= 1
@@ -208,8 +261,5 @@ module Chip8
         end
       end
     end
-  end
-
-  class NoRendererError < StandardError
   end
 end
--- lib/chip_8/renderer.rb
+++ lib/chip_8/renderer.rb
@@ -5,11 +5,36 @@ module Chip8
     WIDTH = 640
     HEIGHT = 320
 
-    def initialize
+    def initialize(chip: chip)
       super(WIDTH, HEIGHT)
+      @timer = 0
+      @chip = chip
     end
 
     def clear
+      @timer = 0
+    end
+
+    def update
+      if @chip.running
+        self.caption = "Something #{@timer}"
+        @chip.run
+
+        if @chip.draw_flag
+          @chip.display.each_with_index do |tile, x, y|
+            color = tile == 0 ? Gosu::Color::BLACK : Gosu::Color::WHITE
+            draw_quad(
+              x, y, color,
+              x + 9, y, color,
+              x, y + 9, color,
+              x + 9, y + 9, color,
+              0
+            )
+          end
+        end
+
+        @timer += 1
+      end
     end
   end
 end
